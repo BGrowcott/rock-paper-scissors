29cecf89795e9dfd4f68293a1a5d344bb2fe1001
Ben <bgrowcott@hotmail.co.uk>
2021-12-19 12:33:00 +0000

    Basic code and rules in place and working

---
+++ index.js
@@ -0,0 +1,58 @@
+var compOp = ["Rock", "Paper", "Scissors"];
+
+var wins = 0;
+var ties = 0;
+var losses = 0;
+
+function playgame() {
+  //User choices
+
+  document.getElementById("rock").onclick = function () {
+    var userchoice = "Rock";
+    window.alert("You choose " + userchoice);
+    computerdecision(userchoice);
+  };
+
+  document.getElementById("paper").onclick = function () {
+    var userchoice = "Paper";
+    window.alert("You choose " + userchoice);
+    computerdecision(userchoice);
+  };
+
+  document.getElementById("scissors").onclick = function () {
+    var userchoice = "Scissors";
+    window.alert("You choose " + userchoice);
+    computerdecision(userchoice);
+  };
+
+  //Computer decision making
+
+  var computerdecision = function (userchoice) {
+    var index = Math.floor(Math.random() * compOp.length);
+    var compchoice = compOp[index];
+    window.alert("The computer chose " + compchoice);
+    result(userchoice, compchoice);
+    console.log(userchoice, compchoice);
+  };
+
+  //Results
+
+  var result = function (userchoice, compchoice) {
+    if (userchoice === compchoice) {
+      ties++;
+      window.alert("It's a tie!");
+    } else if (
+      (userchoice === "Rock" && compchoice === "Scissors") ||
+      (userchoice === "Paper" && compchoice === "Rock") ||
+      (userchoice === "Scissors" && compchoice === "Paper")
+    ) {
+      wins++;
+      window.alert("You win!");
+    } else {
+      losses++;
+      window.alert("You lost!");
+    }
+  };
+}
+
+playgame();
